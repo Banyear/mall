66e5377753022c872384bf8212eedba821f2e3e2
dengpeng <1092518267@qq.com>
2023-09-04 14:54:49 +0800

    初始化

---
+++ order/src/main/java/com/banyear/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.banyear.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
